8f9debc26b734ff335386a58d6ad8b952c188a08
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-07 16:26:34 +0300

    routing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+//enne
+// Route::get('post', function(){
+//     return view('post', [
+//         'post'=> file_get_contents(__DIR__ . '/../resources/posts/my-first-post.html'), //nüüd on $post olemas
+//     ]);
+// });
+//pärast
+Route::get('posts/{post}', function($slug){
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+    if(! file_exists($path)){
+        return redirect("/");
+        // abort(404);
+    };
+
+    $post = file_get_contents($path);
+    return view('post', [
+        'post'=> $post
+    ]);
+})->where('post','[A-z_\-]+');
+//enne oli enne oli ->whereAlpha('post') aga see ei luba - _ routei
