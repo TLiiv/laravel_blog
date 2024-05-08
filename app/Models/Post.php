267d817cd69f27382f93af9bca3b679e6b5b474e
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-08 15:44:34 +0300

    cacheing, sorting home page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,43 +7,52 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File as FacadesFile;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-class Post {
-   public $title;
-   public $excerpt;
-   public $date;
-   public $body;
-   public $slug;
+class Post
+{
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
 
-   public function __construct($title,$excerpt, $date, $body ,$slug) {
-    $this->title = $title;
-    $this->excerpt = $excerpt;
-    $this->date = $date;
-    $this->body = $body;
-    $this->slug = $slug;
-   }
-    public static function all() {
-        $files = FacadesFile::files(resource_path("posts")); 
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+    public static function all()
+    {
+        return cache()->rememberForever('posts.all', function(){
 
-       return collect($files)
-        ->map(function ($file) {
-            return YamlFrontMatter::parseFile($file);
-        })
+        $files = FacadesFile::files(resource_path("posts"));
+
+        return collect($files)
+            ->map(function ($file) {
+                return YamlFrontMatter::parseFile($file);
+            })
             ->map(function ($document) {
-            
-            return new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            );
+
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                    
+                );
+            })
+            ->sortByDesc('date');
         });
-}
+        
+    }
 
     public static function find($slug)
     {
         //of all the blog posts, find the one with a slug that matches the one that was requested
         $posts = static::all();
-        return $posts->firstWhere('slug',$slug);
+        return $posts->firstWhere('slug', $slug);
     }
 }
