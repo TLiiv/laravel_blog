d28a8333b4dbbe4ee7d0dc336317437f3d2e00ad
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-26 15:27:27 +0300

    elequent finished

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File as FacadesFile;
 
@@ -10,7 +11,7 @@ use Illuminate\Support\Facades\File as FacadesFile;
 Route::get('/', function () {
     $posts = Post::all();
      return view('posts',[
-        'posts' => Post::latest()->with('category')->get()
+        'posts' => Post::latest()->with('category','author')->get()
     ]);
 
 });
@@ -42,6 +43,12 @@ Route::get('posts/{post:slug}', function(Post $post){
 
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts',[
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category'.'author'])
+    ]);
+});
+
+Route::get('authors/{author:username}',function(User $author){
+    return view('posts',[
+        'posts' => $author->posts->load(['category'.'author'])
     ]);
 });
