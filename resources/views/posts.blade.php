18a8b462eadbff4b2749df0aebb365a444749cc1
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-26 22:31:12 +0300

    new body with tailwind

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+{{-- @extends('layout')
 
 
 
@@ -20,4 +20,22 @@
             </div>
         </article>
    @endforeach
-@endsection
+@endsection --}}
+@extends('layout')
+@include('_posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card/>
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card/>
+        <x-post-card/>
+
+
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card/>
+        <x-post-card/>
+        <x-post-card/>
+    </div>
+</main>
