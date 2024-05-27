526a2f9b8c3b41e88e4f7d7197465af30da2a15b
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-27 23:40:44 +0300

    dynamic dropdown category menu

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,17 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File as FacadesFile;
 
 
+// Route::get('/', function () {
+//     $posts = Post::all();
+//      return view('posts',[
+//         'posts' => Post::latest()->with('category','author')->get()
+//     ]);
+// });
 Route::get('/', function () {
-    $posts = Post::all();
      return view('posts',[
-        'posts' => Post::latest()->with('category','author')->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
-
 });
 
 
@@ -43,12 +48,21 @@ Route::get('posts/{post:slug}', function(Post $post){
 
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts',[
-        'posts' => $category->posts->load(['category'.'author'])
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
+
     ]);
 });
 
+// Route::get('authors/{author:username}',function(User $author){
+//     return view('posts',[
+//         'posts' => $author->posts->load(['category'.'author'])
+//     ]);
+// });
 Route::get('authors/{author:username}',function(User $author){
     return view('posts',[
-        'posts' => $author->posts->load(['category'.'author'])
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
