f99f1252b70c082ef0d590ac4ce3f4ab273ba508
Tarmo <liivamagi.tarmo@gmail.com>
2024-05-28 23:08:34 +0300

    small tweaks

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -5,7 +5,7 @@
         {{$trigger}}
     </div>
 {{-- links --}}
-    <div x-show="show" class="py-2 absolute bg-gray-200 w-full z-50 mt-2 rounded-xl"style="display:none">
+    <div x-show="show" class="py-2 absolute bg-gray-200 w-full z-50 mt-2 rounded-xl overflow-auto max-h-52"style="display:none">
         {{$slot}}
     </div>
 </div>
