60154fb92e93b438a3819dd71acd70beab34a0cd
Tarmo <liivamagi.tarmo@gmail.com>
2024-06-29 22:58:44 +0300

    search finished

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,10 +12,10 @@ class Post extends Model
     //protected $guarded = ['id'];
 
 public function scopeFilter($query, array $filters){
-    if ($filters['search'] ?? false) {
-        $query->where('title', 'like', '%' . request('search') . '%')
-              ->orWhere('body', 'like', '%' . request('search') . '%');
-    }
+    $query->when($filters['search'] ?? false, function($query, $search) {
+        $query->where('title', 'like', '%' . $search . '%')
+              ->orWhere('body', 'like', '%' . $search . '%');
+    });
 
 }
 
