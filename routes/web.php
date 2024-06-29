194b3efe5b673d74aa3bda5c372d0bf5e25103fa
Tarmo <liivamagi.tarmo@gmail.com>
2024-06-29 14:59:03 +0300

    added search

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
@@ -8,43 +9,10 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 use Illuminate\Support\Facades\File as FacadesFile;
 
 
-// Route::get('/', function () {
-//     $posts = Post::all();
-//      return view('posts',[
-//         'posts' => Post::latest()->with('category','author')->get()
-//     ]);
-// });
-Route::get('/', function () {
-     return view('posts',[
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-
-
-    // ddd($document->body());
-    // $posts = Post::all();
-    // return view('posts',[
-    //     'posts' => $posts
-    // ]);
-
-
-// Route::get('posts/{post}', function($id){
-// //Find a post by its slug and pass it to a view called "post"
-// $post = Post::findOrFail( $id );
-// return view('post',[
-//     'post' => Post::findOrFail($id)
-// ]);
-// });
+Route::get('/', [PostsController::class,'index'])->name('home');
 
 //Koos route model bindinguga
-Route::get('posts/{post:slug}', function(Post $post){
-    //Find a post by its slug and pass it to a view called "post"
-    return view('post',[
-        'post' => $post
-    ]);
-    });
+Route::get('posts/{post:slug}', [PostsController::class,'show']);
 
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts',[
