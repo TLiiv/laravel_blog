c8e769fd7c96dd0e9e2d2067d87f1edfb9dd8818
Tarmo <liivamagi.tarmo@gmail.com>
2024-07-21 18:18:53 +0300

    refactor for query search and bug fix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,40 +8,49 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title','excerpt', 'body','slug','category_id'];
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
     //protected $guarded = ['id'];
 
-    public function scopeFilter($query, array $filters) {
-        $query->when($filters['search'] ?? false, function($query, $search) {
-            $query->where(function($query) use ($search) {
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when(
+            $filters['search'] ?? false,
+            fn ($query, $search) =>
+            $query->where(
+                fn ($query) =>
                 $query->where('title', 'like', '%' . $search . '%')
-                      ->orWhere('body', 'like', '%' . $search . '%');
-            });
-        });
+                    ->orWhere('body', 'like', '%' . $search . '%')
+            )
+        );
+        $query->when(
+            $filters['category'] ?? false,
+            fn ($query, $category) =>
+            $query->whereHas(
+                'category',
+                fn ($query) =>
+                $query->where('slug', $category)
 
-        $query->when($filters['category'] ?? false, function($query, $category) {
-            // $query->whereExists(function($query) use ($category) {
-            //     $query->from('categories')
-            //           ->whereColumn('categories.id', 'posts.category_id')
-            //           ->where('categories.slug', $category);
-            // });
-            $query->whereHas('category',function($query) use ($category){
-                $query->where('slug',$category);
-            });
-        });
-        $query->when($filters['author'] ?? false, function($query, $author) {
-            $query->whereHas('author',function($query) use ($author){
-                $query->where('username',$author);
-            });
-        });
+            )
+        );
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
+                $query->where('username', $author)
 
-        return $query;
+            )
+        );
     }
 
-    public function category(){
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
-    public function author(){
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id'); // peab eraldi välja kirjutama sest laravel eeldab, et on author_id
     }
 }
