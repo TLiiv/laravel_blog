ef5f75289af98a09dddd3148fc154083e5edfaa1
Tarmo <liivamagi.tarmo@gmail.com>
2024-07-26 15:15:35 +0300

    register form

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class,'index'])->name('home');
 
 //Koos route model bindinguga
 Route::get('posts/{post:slug}', [PostsController::class,'show']);
+
+Route::get('register',[RegisterController::class,'create']);
+Route::post('register',[RegisterController::class,'store']);
 
 
