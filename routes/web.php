8ab25fdf07aadea823e40d46ac2c06148641fe41
Tarmo <liivamagi.tarmo@gmail.com>
2024-07-29 14:26:32 +0300

    login and logout

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class,'index'])->name('home');
@@ -9,7 +10,9 @@ Route::get('/', [PostsController::class,'index'])->name('home');
 //Koos route model bindinguga
 Route::get('posts/{post:slug}', [PostsController::class,'show']);
 
-Route::get('register',[RegisterController::class,'create']);
-Route::post('register',[RegisterController::class,'store']);
+Route::get('register',[RegisterController::class,'create'])->middleware('guest');
+Route::post('register',[RegisterController::class,'store'])->middleware('guest');
+
+Route::post('logout',[SessionsController::class,'destroy']);
 
 
