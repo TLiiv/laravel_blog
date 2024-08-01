67aed64ad77adb12146aa36c6d1b83db34f70a09
Tarmo <liivamagi.tarmo@gmail.com>
2024-08-01 18:21:06 +0300

    comment factory and dynamic comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -44,7 +44,10 @@ class Post extends Model
         );
     }
 
-
+    public function comments()
+    {
+        return $this->hasMany(Comment::class); 
+    }
     public function category()
     {
         return $this->belongsTo(Category::class);
