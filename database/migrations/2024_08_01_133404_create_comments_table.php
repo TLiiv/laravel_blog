76c6b14e9b38048b2584fa961aaa9ccb3ec4324c
Tarmo <liivamagi.tarmo@gmail.com>
2024-08-01 17:29:54 +0300

    comments table

--- database/migrations/2024_08_01_133404_create_comments_table.php
+++ database/migrations/2024_08_01_133404_create_comments_table.php
@@ -11,25 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id');
-            $table->string('slug')->unique();
-            $table->string('title');
-            $table->text('excerpt');
+            $table->foreignId('post_id')->references('id')->on('posts')->cascadeOnDelete;
+            $table->foreignId('user_id')->references('id')->on('posts')->cascadeOnDelete;
             $table->text('body');
             $table->timestamps();
-            $table->timestamp('published_at')->nullable();
         });
     }
-
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
