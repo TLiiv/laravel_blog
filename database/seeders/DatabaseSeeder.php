bc304535ad650d2effa823458a2360b71a7218b8
Tarmo <liivamagi.tarmo@gmail.com>
2024-08-15 20:15:09 +0300

    factory fix, add thumbnail on post functionality and dynamic thumbnails

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,68 +2,25 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
         $user = User::factory()->create([
             'name' => 'John Doe'
         ]);
-        Post::factory(10)->create([
-            'user_id'=> $user->id
+
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-        // $user = User::factory()->create();
-
-
-        // $personal = Category::create([
-        //     'name' => 'Personal',
-        //     'slug' => 'personal'
-        // ]);
-
-        // $family = Category::create([
-        //     'name' => 'Family',
-        //     'slug' => 'family'
-        // ]);
-
-        // $work = Category::create([
-        //     'name' => 'Work',
-        //     'slug' => 'work'
-        // ]);
-
-
-        // Post::create([
-        //     'user_id' => $user->id,
-        //     'category_id' => $family->id,
-        //     'title' => 'My Family Post',
-        //     'excerpt' => 'Excerpt for my family post',
-        //     'body' => '<p>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>',
-        //     'slug' => 'my-family-post-1'
-        // ]);
-        // Post::create([
-        //     'user_id' => $user->id,
-        //     'category_id' => $work->id,
-        //     'title' => 'My Work Post',
-        //     'excerpt' => 'Excerpt for my work post',
-        //     'body' => '<p>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet</p>',
-        //     'slug' => 'my-work-post'
-        // ]);
-        // Uncomment this if you want to create a specific user
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,10 @@
 @extends('layout')
 
 @section('content')
-    <x-panel class="max-w-sm mx-auto">
-    <section class="px-6 py-8">
-       <form action="/admin/posts" method="post">
+    <section class="py-8 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">Publish New Post</h1>
+    <x-panel >
+       <form action="/admin/posts" method="post" enctype="multipart/form-data">
        @csrf
        <div class="mb-6">
         <label class="block-mb2 uppercase font-bold text-xs text-gray-700" for="title">
@@ -34,6 +35,17 @@
     @enderror
     </div>
 
+    <div class="mb-6">
+        <label class="block-mb2 uppercase font-bold text-xs text-gray-700" for="thumbnail">
+            Thumbnail
+        </label>
+        <input
+        class="border border-gray-400 p-2 w-full"
+        type="file" name="thumbnail" id="thumbnail" required  value="{{old('thumbnail')}}"/>
+        @error('title')
+        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+    @enderror
+    </div>
 
         <div class="mb-6">
             <label class="block-mb2 uppercase font-bold text-xs text-gray-700" for="excerpt">
