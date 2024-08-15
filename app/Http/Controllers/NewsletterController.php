df928f596f94105e5b417d2dcdc75607dcc66923
Tarmo <liivamagi.tarmo@gmail.com>
2024-08-15 14:06:45 +0300

    newsletter interface and secondary subscription service provider

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -3,24 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Services\Newsletter;
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+
+use Exception;
 
 class NewsletterController extends Controller
 {
     public function __invoke(Newsletter $newsletter)
     {
+
         request()->validate(['email' => 'required|email']);
-
-        $mailchimp = new \MailchimpMarketing\ApiClient();
-
-        $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => config('services.mailchimp.server'),
-    ]);
     try{
         $newsletter->subscribe(request('email'));
-    }catch(\Exception $e){
+    }catch(Exception $e){
         throw ValidationException::withMessages([
             'email' => 'This email could not be added to our newsletter list.'
         ]);
